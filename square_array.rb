ebc57cda7e08815697ddc000c0b1211929f612e8
Alianny Melina Perez <amialejan@gmail.com>
2018-09-03 04:13:51 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   # your code here
-  array = [1,2,3,4] 
-  array.each(array) 
-  array.square = [1,4,9,16]
+  numbers = [1,2,3,4] 
+  array.each 
+  numbers.square = [1,4,9,16]
 end
